6b8207809d2cb7ce82b80f10f0b21f37de47b194
rajatkarmokar2 <rajatkarmokar2@gmail.com>
2026-07-30 17:51:10 +0530

    fix: build route fix v1

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,19 @@ import { createRoot } from "react-dom/client";
 import '@mantine/core/styles.css';
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import { MantineProvider } from "@mantine/core";
 
+const routerBasename = import.meta.env.BASE_URL.replace(/\/$/, "") || "/";
+
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <MantineProvider>
-      <BrowserRouter>
+      <HashRouter basename={routerBasename}>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </MantineProvider>
   </Provider>,
 );
